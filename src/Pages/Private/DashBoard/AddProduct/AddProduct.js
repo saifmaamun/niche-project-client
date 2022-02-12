30e436332d75168150abc8cad6146958c1f8e06c
saif maamun <sadifmamun@gmail.com>
2022-02-13 04:56:08 +0600

    add products done

--- src/Pages/Private/DashBoard/AddProduct/AddProduct.js
+++ src/Pages/Private/DashBoard/AddProduct/AddProduct.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 const AddProduct = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+
+    const onSubmit = data => {
+        
+        axios.post('http://localhost:5000/products', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert('Added Successfully')
+                    console.log(data);
+                    reset()
+                }
+            })
+    };
+
+
     return (
-        <div>
-            <h1>add product</h1>
+        <div className="add-product">
+            <h1>Add A Car</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name")} placeholder='Model No' />
+                <textarea {...register("description")} placeholder='Description' />
+                <input {...register("hints")} placeholder='Summery' />
+                <input {...register("img")} placeholder='Image' />
+                <input type="number" {...register("price")} placeholder='Price' />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
