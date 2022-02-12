d6c7a459056a8dd4818ba1f49df7309395a29622
saif maamun <sadifmamun@gmail.com>
2022-02-13 05:31:39 +0600

    all orders all products done

--- src/Pages/Private/DashBoard/ManageAllOrders/ManageAllOrders.js
+++ src/Pages/Private/DashBoard/ManageAllOrders/ManageAllOrders.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import { Button, Container, Paper, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 const ManageAllOrders = () => {
+    const [boughtItems, setBoughtItems] = useState([]);
+
+
+    useEffect(() => {
+        fetch('https://arcane-badlands-01231.herokuapp.com/purcheased')
+            .then(res => res.json())
+            .then(data => setBoughtItems(data))
+    }, [boughtItems]);
+
+
+    const handleDelete = id => {
+        const url = `https://arcane-badlands-01231.herokuapp.com/purcheased/${id}`
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    alert('this will be deleted')
+                    const remaining = boughtItems.filter(boughtItem => boughtItem._id !== id)
+                    setBoughtItems(remaining)
+                }
+            })
+    }
+
+
     return (
-        <div>
-            <h1>manage all orders</h1>
-        </div>
+        <Container>
+            <h1>All Purchase</h1>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Model</TableCell>
+                            <TableCell align="right">Address</TableCell>
+                            <TableCell align="right">Phone Number</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                        </TableRow>
+                        {boughtItems.map((boughtItem) => (
+                            <TableRow
+                                key={boughtItem._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {boughtItem.userName}
+                                </TableCell>
+                                <TableCell align="right">{boughtItem.productName}</TableCell>
+                                <TableCell align="right">{boughtItem.address}</TableCell>
+                                <TableCell align="right">{boughtItem.number}</TableCell>
+                                <TableCell align="right">{boughtItem.price}</TableCell>
+                                <TableCell align="right"><Button className="btn btn-dark fw-bold" onClick={() => handleDelete(boughtItem._id)}>Remove</Button></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableHead>
+                </Table>
+            </TableContainer>
+        </Container>
     );
 };
 
